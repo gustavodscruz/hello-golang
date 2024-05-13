882f5d97a72d28074c1f687694df637eb999e082
gustavodscruz <gustavodiasdsc@gmail.com>
2024-05-13 13:48:08 -0300

    first commit

---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+
+func main(){
+	var nome string = "Douglas"
+	var idade int
+	var versao float32 = 1.1
+	fmt.Println("Olá sr", nome, "sua idade é", idade)
+	fmt.Println("Esse programa está na versão", versao)
+}
